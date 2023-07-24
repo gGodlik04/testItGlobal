535e18f9e86c36d68aca21f9ae7fa2c41aa65001
Eduard Galikhanov <godlik04@mail.ru>
2023-07-25 01:21:09 +0300

    app ready to dev

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-      </header>
+      privet
     </div>
   );
 }
